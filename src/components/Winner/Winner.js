cecf1477a2a06411cfa883530ce71c2c461e7247
josepplloo <josepplloo@gmail.com>
2019-03-25 03:54:48 -0500

    Update Bubbles

--- src/components/Winner/Winner.js
+++ src/components/Winner/Winner.js
@@ -22,31 +22,19 @@ export default class Winner extends React.Component {
 
   render() {
     const winner = this.state.gameover;
-    
+    const bubblesArray = Array(9).fill(0)
     return (  
       <Fragment>
         <div className="animation__background">
           <ul className="bg__bubbles--rigth">
-            <li className="bg__bubble bg__bubble1"></li>
-            <li className="bg__bubble bg__bubble2"></li>
-            <li className="bg__bubble bg__bubble3"></li>
-            <li className="bg__bubble bg__bubble4"></li>
-            <li className="bg__bubble bg__bubble5"></li>
-            <li className="bg__bubble bg__bubble6"></li>
-            <li className="bg__bubble bg__bubble7"></li>
-            <li className="bg__bubble bg__bubble8"></li>
-            <li className="bg__bubble bg__bubble9"></li>
+          { bubblesArray.map((_, index) => {
+              return <li key={index} className={`bg__bubble bg__bubble${index+1}`}></li>
+            }) }
           </ul>
           <ul className="bg__bubbles--left">
-            <li className="bg__bubble bg__bubble1"></li>
-            <li className="bg__bubble bg__bubble2"></li>
-            <li className="bg__bubble bg__bubble3"></li>
-            <li className="bg__bubble bg__bubble4"></li>
-            <li className="bg__bubble bg__bubble5"></li>
-            <li className="bg__bubble bg__bubble6"></li>
-            <li className="bg__bubble bg__bubble7"></li>
-            <li className="bg__bubble bg__bubble8"></li>
-            <li className="bg__bubble bg__bubble9"></li>
+             { bubblesArray.map((_, index) => {
+              return <li key={index} className={`bg__bubble bg__bubble${index+1}`}></li>
+            }) }
           </ul>
         </div>
         <h1>The winner is {winner}</h1>
